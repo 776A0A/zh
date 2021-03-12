6e128d86edbd06897891115406778a450253be54
776A0A <har3dy@icloud.com>
2021-03-12 18:32:48 +0800

    feat(GlobalHeader): 添加全局顶部栏

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav
+        class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4"
+    >
+        <a href="#" class="navbar-brand">zh</a>
+        <ul v-if="!user.isLogin"></ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface UserProps {
+    isLogin: boolean
+    name?: string
+    id?: number
+}
+
+export default defineComponent({
+    name: 'GlobalHeader',
+    props: {
+        user: {
+            type: Object as PropType<UserProps>,
+            required: true,
+        },
+    },
+})
+</script>
+
+<style lang="less"></style>
