271cb5847c8d0d0311591d5bb044b05e8a7f37dc
darenome <har3dy@icloud.com>
2021-03-19 20:01:26 +0800

    feat(router): 添加路由首位，处理未登录或者已登录重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,18 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import store from '@/store'
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
-        component: import('@/views/Home.vue'),
+        component: () => import('@/views/Home.vue'),
     },
     {
         path: '/login',
         name: 'Login',
         component: () =>
             import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+        meta: { redirectIfLogin: true },
     },
     {
         path: '/column/:id',
@@ -22,12 +24,18 @@ const routes: Array<RouteRecordRaw> = [
         path: '/create',
         name: 'CreatePost',
         component: import('@/views/CreatePost.vue'),
+        meta: { requiredLogin: true },
     },
 ]
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
+})
+
+router.beforeEach((to, from) => {
+    if (to.meta.requiredLogin && !store.state.user.isLogin) return '/login'
+    if (to.meta.redirectIfLogin && store.state.user.isLogin) return '/'
 })
 
 export default router
